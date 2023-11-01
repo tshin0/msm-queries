020f9031474adbfde3a08c8ea95fd1819ef57796
tshin0 <146213491+tshin0@users.noreply.github.com>
2023-11-01 16:13:30 +0000

    added actors list and details

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,4 +4,13 @@ class ActorsController < ApplicationController
   render({ :template => "actor_templates/list"})
   end
 
+  def show
+    the_id = params.fetch("the_id")
+    matching_records = Actor.where({ :id => the_id})
+
+    @the_actor = matching_records.at(0)
+
+    render({ :template => "actor_templates/details"})
+    end
+
 end
